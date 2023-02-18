c22696f67134e8086abd919df5e7c1f26c5fd836
Angel Adrian de la Rosa Rosario <98406166+AngelNFt@users.noreply.github.com>
2023-02-18 00:04:36 -0600

    fgnfgn

---
+++ ejercicio_1.py
@@ -0,0 +1,7 @@
+#funcion del nombre
+nombre = input("¿Cual es tu nombre? ")
+#funcion del nunero
+num_entero = input("Introduce un número entero: ")
+#resultado de funcion
+print((nombre + "\n") * int(num_entero))
+    
